44f8470988aa11ba700b9bbae2dceb4d1a499d7b
Pedro <pedrosabino@outlook.com>
2022-09-17 11:25:25 -0300

    Sugestoes e Stories componentizados e itemizados

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,3 @@
-
-
 export default function Navbar() {
     return (
         <div class="navbar">
@@ -7,7 +5,7 @@ export default function Navbar() {
                 <div class="logo">
                     <ion-icon name="logo-instagram"></ion-icon>
                     <div class="separador"></div>
-                    <img src="assets/img/logo.png" />
+                    <img src="assets/img/logo.png" alt="logo" />
                 </div>
 
                 <div class="logo-mobile">
@@ -15,7 +13,7 @@ export default function Navbar() {
                 </div>
 
                 <div class="instagram-mobile">
-                    <img src="assets/img/logo.png" />
+                    <img src="assets/img/logo.png" alt="logo" />
                 </div>
 
                 <div class="pesquisa">
---
+++ src/components/Usuario.js
@@ -0,0 +1,16 @@
+
+
+export default function Usuario() {
+    return (
+        <div class="usuario">
+            <img src="assets/img/catanacomics.svg" alt="user pic"/>
+            <div class="texto">
+                <strong>catanacomics</strong>
+                <span>
+                    Catana
+                    <ion-icon name="pencil"></ion-icon>
+                </span>
+            </div>
+        </div>
+    )
+}
